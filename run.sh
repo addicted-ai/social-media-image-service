ce94c516f4b1c269944ee76b7d965059b06b0750
Addicted <ashutoshprusty29@gmail.com>
2021-06-28 03:29:21 +0530

    selenium+chrome

--- run.sh
+++
@@ -1,7 +0,0 @@
-# Script to run flask app without docker
-
-source venv/bin/activate  # Activate virtual env
-export FLASK_APP=app.py
-export WERKZEUG_DEBUG_PIN=off
-export FLASK_ENV='development'
-flask run --host=0.0.0.0 --port=5001
